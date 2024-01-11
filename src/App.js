c924914ba353145d1af52bf5234019517e78dc80
jisooooooooooo <dlawltn0708@naver.com>
2024-01-11 22:45:59 +0900

    Feat: 회원정보박스 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import MyPage from "./pages/MyPage";
 function App() {
   return (
     <div className="App">
       <div className="bg-red-400 text-white">Tailwind CSS</div>
+      <MyPage/>
     </div>
   );
 }
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+//상단바+메뉴바까지 같이 생각해서 마진 설정
+export const MyPage = () => {
+  return (
+    <div className="grid place-items-center">
+      <div className="mt-[6.315rem] w-[95.4375rem] h-[24.0625rem] rounded-[1.125rem] shadow-[0_0_45px_0_rgba(0,0,0,0.2)] backdrop-sepia-74.99999237060547 bg-white-500/70">회원정보박스</div>
+    </div>
+  );
+};
+export default MyPage;
