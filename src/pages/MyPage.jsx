0dad625651c6820fe84c3b403b125ec49bd44498
jisooooooooooo <dlawltn0708@naver.com>
2024-01-13 00:16:07 +0900

    Feat: 완료된 리뷰

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -182,9 +182,15 @@ export const MyPage = () => {
             </div>
           </div>
 
-          <div className="w-[74.625rem] mt-[5.82rem] border-[#000000] border-b pb-[1rem] text=[1.25rem] font-semibold">
+          <div className="w-[74.625rem] mt-[5.82rem] mb-[1.06rem] border-[#000000] border-b pb-[1rem] text=[1.25rem] font-semibold">
             완료된 리뷰
           </div>
+          <div className="w-[14.4125rem] h-[20.1875rem] mb-[9.71rem] p-[0.44rem] rounded-[0.19538rem] shadow-[0_0_6.253px_0_rgba(0,0,0,0.25)]">
+              <div className="rounded-[0.19538rem] mb-[0.95rem] w-[13.36881rem] h-[11.23306rem] bg-[#FFEDED]">이미지</div>
+              <div  style={{ fontSize: "0.9375rem" }} className="text-[#545454] leading-[1.17238rem] mb-[0.5rem] w-[12.9375rem] h-[4.8125rem] font-bold">데이트 코스로 딱! 여의도역 5분 거리 스끼다시 맛있는 집!!</div>
+              <div style={{ fontSize: "0.62525rem" }} className="text-[#FF0069] leading-[1.17238rem]">여의도 또가횟집</div>
+          </div>
+
         </div>
       </div>
     </div>
